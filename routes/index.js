c53f09c26e91ff41c341f37839afac2f8ea0a897
Riley Dallas <rldaggie@gmail.com>
2016-08-26 11:45:53 -0500

    Setup twit config

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
